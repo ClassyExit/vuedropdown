7b143cee66120c4ae8eb295347835492c0a58344
Alex <alexgenriech10@gmail.com>
2022-09-22 21:58:12 -0700

    ...

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <DropDown :mode="'single'" :options="options" />
+  <button @click="">CLick</button>
+  <h1>{{}}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import DropDown from "./components/DropDown.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { DropDown },
+  setup() {
+    const options = [
+      { label: "First", value: 1 },
+      { label: "Second", value: 2 },
+      { label: "Third", value: 3 },
+      { label: "Fourth", value: 4 },
+      { label: "Fifth", value: 5 },
+    ];
+
+    return { options };
+  },
+});
+</script>
+
+<style></style>
---
+++ src/components/DropDown.vue
@@ -0,0 +1,192 @@
+<template>
+  <div
+    class="container"
+    tabindex="0"
+    @click="isOpen = !isOpen"
+    @blur="isOpen = false"
+  >
+    <span v-if="props.mode === 'single'" class="value">
+      {{ isSelected.label }} + {{ props.selectedValue?.label }}
+    </span>
+    <span v-if="props.mode === 'multi'" class="value">{{ isSelected }}</span>
+    <button @click.stop @click="clearOption" class="clear-btn">&times;</button>
+    <div class="divider"></div>
+    <div class="caret"></div>
+    <ul class="options" :class="isOpen ? 'show' : ''">
+      <li
+        @click.stop
+        @click="
+          isOpen = false;
+          selectOption(option);
+        "
+        class="option"
+        v-for="option in props.options"
+        :key="option.value"
+        :class="isOptionSelected(option) ? 'selected' : ''"
+      >
+        {{ option.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType, reactive, ref } from "vue";
+
+type SelectOption = {
+  label: string;
+  value: string | number;
+};
+
+const props = defineProps({
+  mode: String, // [Single | Multi]
+  selectedValue: { type: Object as PropType<SelectOption> | null }, // Value is an object of Select Option
+  options: { type: Array as PropType<Array<SelectOption>> }, // Options is an array of SelectOption objects
+});
+
+let isOpen = ref(false);
+
+interface Selection {
+  label: any;
+  value: any;
+}
+
+const isSelected: Selection = reactive({
+  label: "Click to select option",
+  value: "",
+});
+
+const selectOption = (option: SelectOption) => {
+  isSelected.label = option.label;
+  isSelected.value = option.value;
+};
+
+const clearOption = () => {
+  isSelected.label = null;
+  isSelected.value = null;
+};
+
+const isOptionSelected = (option: SelectOption) => {
+  return option.value === isSelected.value;
+};
+
+// TODO: Find a way to pass data out from
+// Solution 1: Pass data through store (like pinia)
+// Solution 2:
+</script>
+
+<style scoped>
+.container {
+  position: relative;
+  width: 20em;
+  min-height: 1.5em;
+  border: 0.05em solid #777;
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+  padding: 0.5em;
+  border-radius: 0.25em;
+  outline: none;
+}
+
+.container:focus {
+  border-color: hsl(200, 100%, 50%);
+}
+
+.value {
+  flex-grow: 1;
+  display: flex;
+  gap: 0.5em;
+  flex-wrap: wrap;
+}
+
+.clear-btn {
+  background: none;
+  color: #777;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 0;
+  font-size: 1.25em;
+}
+
+.clear-btn:focus,
+.clear-btn:hover {
+  color: #333;
+}
+
+.divider {
+  background-color: #777;
+  align-self: stretch;
+  width: 0.05em;
+}
+
+.caret {
+  translate: 0 25%;
+  border: 0.25em solid transparent;
+  border-top-color: #777;
+}
+
+.options {
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: none;
+  max-height: 15em;
+  overflow-y: auto;
+  border: 0.05em solid #777;
+  border-radius: 0.25em;
+  width: 100%;
+  left: 0;
+  top: calc(100% + 0.25em);
+  background-color: white;
+  z-index: 100;
+}
+
+.options.show {
+  display: block;
+}
+
+.option {
+  padding: 0.25em 0.5em;
+  cursor: pointer;
+}
+
+.option.selected {
+  background-color: hsl(200, 100%, 70%);
+}
+
+.option.highlighted {
+  background-color: hsl(200, 100%, 50%);
+  color: white;
+}
+
+.option-badge {
+  display: flex;
+  align-items: center;
+  border: 0.05em solid #777;
+  border-radius: 0.25em;
+  padding: 0.15em 0.25em;
+  gap: 0.25em;
+  cursor: pointer;
+  background: none;
+  outline: none;
+}
+
+.option-badge:hover,
+.option-badge:focus {
+  background-color: hsl(0, 100%, 90%);
+  border-color: hsl(0, 100%, 50%);
+}
+
+.option-badge:hover > .remove-btn,
+.option-badge:focus > .remove-btn {
+  color: hsl(0, 100%, 50%);
+}
+
+.option-badge > .remove-btn {
+  font-size: 1.25em;
+  color: #777;
+}
+</style>
